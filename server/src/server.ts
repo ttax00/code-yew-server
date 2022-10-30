5d84e7ecbd6d61fad007d7050af6d4dc24ebb11d
TechTheAwesome <techtheawesome01@gmail.com>
2022-10-30 19:12:12 +0900

    REMOVE unused import

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,6 @@
  * ------------------------------------------------------------------------------------------ */
 import {
 	createConnection,
-	TextDocuments,
 	ProposedFeatures,
 	InitializeParams,
 	TextDocumentSyncKind,
