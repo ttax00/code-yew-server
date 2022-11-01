94cc0c00eda64a1bce744f9f7230d19712a19b14
TechTheAwesome <techtheawesome01@gmail.com>
2022-11-01 15:47:25 +0900

    REMOVE example's MIT license notice

--- client/src/test/index.ts
+++ client/src/test/index.ts
@@ -1,7 +1,3 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
 import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
