abdffe872c8c27e1523a906d45686ee90d45d4a9
TechTheAwesome <techtheawesome01@gmail.com>
2023-01-18 21:51:14 +0900

    REFACTOR move clientOptions out of activate func

---
+++ client/src/clientOptions.ts
@@ -0,0 +1,123 @@
+import { commands, Uri, CompletionList, TextDocument, DocumentSymbol, Hover, WorkspaceEdit, Range, Position, TextEdit, CompletionItemKind } from 'vscode';
+
+import {
+	LanguageClientOptions
+} from 'vscode-languageclient/node';
+import { flattenDocumentSymbols, getHTMLVirtualContent, getSymbolShortName, isInsideHTMLRegion, isValidRustYew } from './embeddedHTML';
+import { virtualDocumentContents } from './extension';
+
+
+function vdocUri(document: TextDocument) {
+	const originalUri = document.uri.toString(true);
+	const vdocUriString = `embedded://html/${encodeURIComponent(
+		originalUri
+	)}.html`;
+	const vUri = Uri.parse(vdocUriString);
+
+	virtualDocumentContents.set(vUri.toString(true), getHTMLVirtualContent(document.getText()));
+	// virtualDocumentContents.set(vUri.toString(), "<div></div > ");
+	return vUri;
+}
+
+// Options to control the language client
+export const clientOptions: LanguageClientOptions = {
+	// Register the server for rust documents
+	documentSelector: [{ scheme: 'file', language: 'rustyew' }, { scheme: 'file', language: 'rust' }],
+	middleware: {
+		async provideCompletionItem(document, position, context, token, next) {
+			// If not in `html! {}`, do not perform request forwarding
+			if (!isInsideHTMLRegion(document.getText(), document.offsetAt(position))) {
+				return await next(document, position, context, token);
+			}
+			const result = await commands.executeCommand<CompletionList>(
+				'vscode.executeCompletionItemProvider',
+				vdocUri(document),
+				position,
+				context.triggerCharacter
+			);
+			// filter valid items
+			result.items = result.items.filter((i) => i.kind === CompletionItemKind.Property
+				|| i.kind === CompletionItemKind.Value);
+			result.items = result.items.filter((i) => !i.label.toString().match(/aria-.*/g));
+
+			return result;
+		},
+		async prepareRename(document, position, token, next) {
+			// If not in `html! {}`, do not perform request forwarding
+			if (!isInsideHTMLRegion(document.getText(), document.offsetAt(position))) {
+				return await next(document, position, token);
+			}
+			const result = await commands.executeCommand<Range>(
+				'vscode.prepareRename',
+				vdocUri(document),
+				position
+			);
+			return result;
+		},
+		async provideRenameEdits(document, position, newName, token, next) {
+			// If not in `html! {}`, do not perform request forwarding
+			if (!isInsideHTMLRegion(document.getText(), document.offsetAt(position))) {
+				return await next(document, position, newName, token);
+			}
+			const edit: WorkspaceEdit = new WorkspaceEdit();
+			const symbols = await commands.executeCommand<DocumentSymbol[]>(
+				'vscode.executeDocumentSymbolProvider',
+				vdocUri(document),
+			);
+
+			const flat = flattenDocumentSymbols(symbols);
+			const symbol = flat.filter((s) => {
+				return s.range.start.line === position.line || s.range.end.line === position.line;
+			}).find((s) => {
+				const { length } = getSymbolShortName(s.name);
+				const { character } = position;
+				const { start, end } = s.range;
+				return ((start.character < character && character <= start.character + length + 1)
+					|| (end.character - length - 1 <= character && character < end.character));
+
+			});
+
+			if (symbol) {
+				const { start, end } = symbol.range;
+				const { length } = getSymbolShortName(symbol.name);
+				edit.set(document.uri, [
+					new TextEdit(new Range(new Position(end.line, end.character - 1 - length),
+						new Position(end.line, end.character - 1),), newName),
+					new TextEdit(new Range(new Position(start.line, start.character + 1),
+						new Position(start.line, start.character + 1 + length)), newName),
+				]);
+			}
+			return edit;
+		},
+		async provideHover(document, position, token, next) {
+			// If not in `html! {}`, do not perform request forwarding
+			if (!isInsideHTMLRegion(document.getText(), document.offsetAt(position))) {
+				return await next(document, position, token);
+			}
+			const result = await commands.executeCommand<Hover[]>(
+				'vscode.executeHoverProvider',
+				vdocUri(document),
+				position,
+			);
+			return result[0];
+		},
+		async provideDocumentSymbols(document, token, next) {
+			if (!isValidRustYew(document.getText())) {
+				return next(document, token);
+			}
+			// FIXME: [1] result is undefined for a long time, perhaps HTML Language Service isn't started yet?
+			// FIXME: [2] HTML document symbol provided are greatly missing!
+			let result: undefined | DocumentSymbol[] = undefined;
+			let count = 0;
+			while (result === undefined && count < 5) {
+				await new Promise(r => setTimeout(r, 1000));
+				result = await commands.executeCommand<DocumentSymbol[]>(
+					'vscode.executeDocumentSymbolProvider',
+					vdocUri(document),
+				);
+				count++;
+			}
+			return flattenDocumentSymbols(result);
+		}
+	}
+};
