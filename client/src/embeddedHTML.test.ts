1a778874a3b016e484248eb9f1033762c448168e
TechTheAwesome <techtheawesome01@gmail.com>
2023-01-22 02:50:08 +0900

    ADD unit tests

--- client/src/embeddedHTML.test.ts
+++ client/src/embeddedHTML.test.ts
@@ -1,12 +1,38 @@
-import { getHTMLVirtualContent, getSymbolShortName, isValidHTMLMacro } from './embeddedHTML';
-
-describe('isValidRustYew', () => {
+import { getHTMLVirtualContent, getSymbolShortName, isInsideHTMLRegion, isValidHTMLMacro } from './embeddedHTML';
+// TODO: ADD jest *test.ts files to typescript compiler ignore
+describe('isValidHTMLMacro', () => {
 	it('should be false without a html! macro', () => {
 		expect(isValidHTMLMacro('fn main() {}')).toBe(false);
 	});
 
 	it('should be true for document with a html macro', () => {
 		expect(isValidHTMLMacro('fn main() { html! {<div></div>}}')).toBe(true);
+	});
+});
+
+describe('isInsideHTMLRegion', () => {
+
+	const test = `html! {<div>{"Hello World"}</div>}`;
+	expect(test.length).toBe(34);
+	expect(test.charAt(12)).toBe('{');
+
+	function arr(from: number, to: number): number[] {
+		return Array.from({ length: to - from + 1 }, (_, i) => i + from);
+	}
+	expect(arr(1, 5)).toStrictEqual([1, 2, 3, 4, 5]);
+
+	it.each([
+		...arr(7, 11), // <div>
+		...arr(27, 32) // </div>
+	])(`it should return true for pos: %p`, (i) => {
+		expect(isInsideHTMLRegion(test, i)).toBe(true);
+	});
+
+	it.each([...arr(0, 6), 	// html! {
+	...arr(12, 26),		   	// {"Hello World"}
+		33					// }
+	])(`it should return false for pos: %p`, (i) => {
+		expect(isInsideHTMLRegion(test, i)).toBe(false);
 	});
 });
 describe('getHTMLVirtualContent', () => {
