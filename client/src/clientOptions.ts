d69af80455e45edf89c820bf6447c9a4da5f772c
TechTheAwesome <techtheawesome01@gmail.com>
2023-01-26 19:58:31 +0900

    ADD working prepareRename for functional component

--- client/src/clientOptions.ts
+++ client/src/clientOptions.ts
@@ -41,11 +41,28 @@ export const clientOptions: LanguageClientOptions = {
 			if (!isInsideHTMLRegion(document.getText(), document.offsetAt(position))) {
 				return await next(document, position, token);
 			}
-			const result = await commands.executeCommand<Range>(
+			const result = await commands.executeCommand<{ range: Range; placeholder: string; }>(
 				'vscode.prepareRename',
 				vdocUri(document),
 				position
 			);
+			// If empty space for vanila HTML.
+			if (result.range.isEmpty) {
+				const line = document.lineAt(position.line);
+				const re = /<\s*(\w+)\s*\/>/g; // matches `< FunctionalComponent />` declaration 
+				for (const match of line.text.matchAll(re)) {
+					const start = match.index + match[0].indexOf(match[1]);
+					const end = start + match[1].length - 1;
+					const range = new Range(position.line, start, position.line, end);
+					if (range.contains(position)) {
+						return {
+							range: new Range(position.line, start, position.line, end),
+							placeholder: match[1],
+						};
+					}
+				}
+				return result;
+			}
 			return result;
 		},
 		async provideRenameEdits(document, position, newName, token, next) {
@@ -53,12 +70,8 @@ export const clientOptions: LanguageClientOptions = {
 				return await next(document, position, newName, token);
 			}
 			const edit: WorkspaceEdit = new WorkspaceEdit();
-			const symbols = await commands.executeCommand<DocumentSymbol[]>(
-				'vscode.executeDocumentSymbolProvider',
-				vdocUri(document),
-			);
+			const flat = await getFlattenSymbols(document);
 
-			const flat = flattenDocumentSymbols(symbols);
 			const symbol = flat.filter((s) => {
 				return s.range.start.line === position.line || s.range.end.line === position.line;
 			}).find((s) => {
@@ -100,14 +113,18 @@ export const clientOptions: LanguageClientOptions = {
 			// FIXME: [1] result is undefined for a long time, perhaps HTML Language Service isn't started yet?
 			// TEMP: try again every 1s, timeout at 5 times.
 
-			let result = undefined;
-			for (let counter = 0; counter < 5 && !result; counter++) {
-				result = await commands.executeCommand<DocumentSymbol[]>(
-					'vscode.executeDocumentSymbolProvider',
-					vdocUri(document),
-				);
-			}
-			return flattenDocumentSymbols(result);
+			return await getFlattenSymbols(document);
 		}
 	}
 };
+
+async function getFlattenSymbols(document: TextDocument): Promise<DocumentSymbol[]> {
+	let result = undefined;
+	for (let counter = 0; counter < 5 && !result; counter++) {
+		result = await commands.executeCommand<DocumentSymbol[]>(
+			'vscode.executeDocumentSymbolProvider',
+			vdocUri(document),
+		);
+	}
+	return flattenDocumentSymbols(result);
+}
